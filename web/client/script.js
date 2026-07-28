41a0fc9efaea3f32b8a6611a1fc4b35e5f59c31f
Pyramid302614 <pyramidstudios.dev@gmail.com>
2026-07-28 11:26:40 -0700

    Nickname setting, WebSocket basic functionality

--- web/client/script.js
+++ web/client/script.js
@@ -1,7 +1,130 @@
-// Splash Screen
-setTimeout(() => {
-    document.getElementById("splash--container").style.opacity = 0;
-    setTimeout(() => {
+const bypassAllWallErrors = false;
+var wallError = false;
+
+if("&&test" == "&" + "&test") {
+
+    document.body.style.backgroundColor = "red";
+    document.getElementById("splash").innerHTML = "<b style='color:white'>( ! ) Template Error ( ! )";
+    wallError = true;
+
+}
+
+if(!wallError || bypassAllWallErrors) {
+
+    document.body.style.backgroundColor = "white";
+
+    // Template storage
+    const chatbox = {
+        ws: "&&chatbox.ws",
+        host: "&&chatbox.host"
+    };
+    const client = {
+        id: "&&client.id",
+        name: "Unknown"
+    };
+
+    // Server
+    async function nicknameSetFromTextbox() {
+        
+        const result = await updateName(document.getElementById("nickname-textbox").value);
+        if(result) {
+            updateLocalName(document.getElementById("nickname-textbox").value);
+            document.getElementById("nickname-textbox").value = "";
+        } else document.getElementById("nickname-textbox").style.backgroundColor = "red";
+
+    }
+    function updateLocalName(name_) {
+
+        document.getElementById("name-display").innerHTML = name_;
+        client.name = name_;
+        
+    }
+    async function fetchName() {
+
+        return await (await fetch(chatbox.host+"/me:"+client.id)).text();
+    
+    }
+    async function updateName(name) {
+
+        if(!ws) {
+            systemMessage("Unable to update name: You are not connected to the websocket!");
+            return false;
+        }
+        ws.send("name>"+name);
+        return true;
+
+    }
+
+    // Websocket
+    var ws;
+    async function connectToWS() {
+
+        ws = new WebSocket("ws://localhost:3000");
+
+        await new Promise(resolve => ws.onopen = resolve);
+
+        ws.send("join>"+client.id);
+
+        document.getElementById("loading-text").innerHTML = "Joining...";
+
+        await new Promise(resolve => setTimeout(resolve,1_000)); // Give it time to process
+
+        const onmessage = (e) => {
+            console.log(e);
+        };
+
+        const onclose = (e) => {
+            systemMessage("The websocket has unexpectedly closed.<br/>Attemping to reconnect...");
+            var i = setInterval(async () => {
+                try {
+                    ws = new WebSocket(chatbox.ws);
+                    await new Promise(resolve => ws.opopen = resolve);
+                    ws.onmessage = onmessage;
+                    ws.onclose = onclose;
+                    systemMessage("Re-established communication with WebSocket.");
+                    clearInterval(i);
+                } catch(ignored) {}
+            },5_000);
+        };
+
+        ws.onmessage = onmessage;
+        ws.onclose = onclose;
+
+    }
+
+    // Content output handling
+    function rawMessage(msg) {
+        document.getElementById("content").innerHTML += `${msg}<br/>`;
+    }
+    function systemMessage(msg) {
+        rawMessage(`<b class='gradient-lr'>${msg}</b>`)
+    }
+    function userMessage(author,msg) {
+        rawMessage(`<b>${author}</b> > ${msg}`);
+    }
+
+    // Splash Screen
+    (async () => {
+
+        await new Promise(resolve => setTimeout(resolve,2_500));
+
+        document.getElementById("loading-text").innerHTML = "Fetching name...";
+        updateLocalName(await fetchName());
+
+        document.getElementById("loading-text").innerHTML = "Connecting to WebSocket...";
+        await connectToWS();
+        
+        document.getElementById("nickname-textbox").addEventListener("keydown",(e) => {
+            if(e.key == "Enter") nicknameSetFromTextbox();
+            document.getElementById("nickname-textbox").style.backgroundColor = "black";
+        });
+
+        // Transition to home
+        await new Promise(resolve => setTimeout(resolve,2_000));
+        document.getElementById("splash--container").style.opacity = 0;
+        await new Promise(resolve => setTimeout(resolve,500));
         document.getElementById("nonsplash--container").style.opacity = 1;
-    }, 500);
-}, 2_000);
+
+    })();
+
+}
--- web/host.js
+++ web/host.js
@@ -1,19 +1,80 @@
 process.addListener("uncaughtException",(e) => console.error(e.message));
 process.addListener("unhandledRejection",(r) => console.error(r.reason));
 
+function ambervars(str,values) {
+    for(ambervar of Object.keys(values)) str = str.replaceAll(`&&${ambervar}`,values[ambervar]);
+    return str;
+}
+
+function ip(req) {
+    return req.socket.remoteAddress;
+}
+function randomClientId() {
+    const existing = Object.values(JSON.parse(require("fs").readFileSync("web/clients.json","utf-8")));
+    var tried = null;
+    while(tried === null || existing.includes(tried)) {
+        tried = Math.floor(Math.random()*10_000);
+    }
+    return tried;
+}
+function clientId(ip) {
+    const data = JSON.parse(require("fs").readFileSync("web/clients.json","utf-8"));
+    if(!Object.keys(data).includes(ip)) {
+        data[ip] = randomClientId();
+        require("fs").writeFileSync("web/clients.json",JSON.stringify(data,null,2),"utf-8");
+        return data[ip];
+    } else {
+        return data[ip];
+    }
+}
+
 const server = require("http").createServer((req,res) => {
+
+    // CORS headers
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method == "OPTIONS") {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+
+    const universalAmbervars = {
+        "test": true,
+        "chatbox.ws": "ws://192.168.0.190:3000",
+        "chatbox.host": "http://192.168.0.190:3000",
+        "client.id": clientId(ip(req))
+    }
+    
     switch(req.url) {
         case "/":
             res.writeHead(200,{"Content-Type":"text/html"});
-            res.end(require("fs").readFileSync("web/client/index.html","utf-8"));
+            res.end(
+                ambervars(
+                    require("fs").readFileSync("web/client/index.html","utf-8"),
+                    universalAmbervars
+                )
+            );
             break;
         case "/styles.css":
             res.writeHead(200,{"Content-Type":"text/css"});
-            res.end(require("fs").readFileSync("web/client/styles.css","utf-8"));
+            res.end(
+                ambervars(
+                    require("fs").readFileSync("web/client/styles.css","utf-8"),
+                    universalAmbervars
+                )
+            );
             break;
         case "/script.js":
-            res.writeHead(200,{"Content-Type":"text/html"});
-            res.end(require("fs").readFileSync("web/client/script.js","utf-8"));
+            res.writeHead(200,{"Content-Type":"application/json"});
+            res.end(
+                ambervars(
+                    require("fs").readFileSync("web/client/script.js","utf-8"),
+                    universalAmbervars
+                )
+            );
             break;
         default:
             res.writeHead(200,{"Content-Type":"text/plain"});
